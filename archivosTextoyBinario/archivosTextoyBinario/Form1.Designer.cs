0c529393831228a0ae11e15c438de58e3da3c2ed
AureAle <32805589+AureAle@users.noreply.github.com>
2018-03-10 23:49:08 -0600

    Add files via upload

---
+++ archivosTextoyBinario/archivosTextoyBinario/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace archivosTextoyBinario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.sFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.opnFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardarXML = new System.Windows.Forms.Button();
+            this.txtApellido1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtApellido2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumCuenta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.Transparent;
+            this.btnAbrir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbrir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnAbrir.Location = new System.Drawing.Point(172, 44);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(82, 32);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(93, 95);
+            this.txtArchivo.Multiline = true;
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ReadOnly = true;
+            this.txtArchivo.Size = new System.Drawing.Size(241, 135);
+            this.txtArchivo.TabIndex = 1;
+            // 
+            // opnFileDialog
+            // 
+            this.opnFileDialog.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(117, 245);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(90, 28);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // btnGuardarXML
+            // 
+            this.btnGuardarXML.BackColor = System.Drawing.Color.Transparent;
+            this.btnGuardarXML.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarXML.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnGuardarXML.Location = new System.Drawing.Point(186, 383);
+            this.btnGuardarXML.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGuardarXML.Name = "btnGuardarXML";
+            this.btnGuardarXML.Size = new System.Drawing.Size(82, 32);
+            this.btnGuardarXML.TabIndex = 4;
+            this.btnGuardarXML.Text = "Guardar";
+            this.btnGuardarXML.UseVisualStyleBackColor = false;
+            this.btnGuardarXML.Click += new System.EventHandler(this.btnGuardarXML_Click);
+            // 
+            // txtApellido1
+            // 
+            this.txtApellido1.Location = new System.Drawing.Point(301, 245);
+            this.txtApellido1.Name = "txtApellido1";
+            this.txtApellido1.Size = new System.Drawing.Size(90, 28);
+            this.txtApellido1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Apellido1: ";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(117, 287);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(90, 28);
+            this.txtEdad.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Edad: ";
+            // 
+            // txtApellido2
+            // 
+            this.txtApellido2.Location = new System.Drawing.Point(301, 287);
+            this.txtApellido2.Name = "txtApellido2";
+            this.txtApellido2.Size = new System.Drawing.Size(90, 28);
+            this.txtApellido2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Apellido2: ";
+            // 
+            // txtNumCuenta
+            // 
+            this.txtNumCuenta.Location = new System.Drawing.Point(117, 332);
+            this.txtNumCuenta.Name = "txtNumCuenta";
+            this.txtNumCuenta.Size = new System.Drawing.Size(90, 28);
+            this.txtNumCuenta.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Num Cuenta: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(449, 448);
+            this.Controls.Add(this.txtNumCuenta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtApellido2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApellido1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGuardarXML);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.btnAbrir);
+            this.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "ArchivosTextoyBinarios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.SaveFileDialog sFileDialog;
+        private System.Windows.Forms.OpenFileDialog opnFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardarXML;
+        private System.Windows.Forms.TextBox txtApellido1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtApellido2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumCuenta;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ archivosTextoyBinario/archivosTextoyBinario/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace archivosTextoyBinario
+{
+    public partial class Form1 : Form
+    {
+        char fstbit, scndbit;
+        int ancho, alto,bytes, bxp;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private bool esBMP(FileStream archivo)
+        {
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(0, SeekOrigin.Begin);
+            fstbit = Convert.ToChar(br.ReadByte());
+            br.BaseStream.Seek(1, SeekOrigin.Begin);
+            scndbit = Convert.ToChar(br.ReadByte());
+            if (fstbit == 'B' && scndbit == 'M')
+            {
+                Console.WriteLine("Marca del fichero: {0}{1}", fstbit, scndbit);
+                //txtArchivo.Text += "es bmp";
+
+                return true;
+            }
+            else
+            {
+                br.Close();
+                archivo.Close();
+                Console.WriteLine("no es");
+                txtArchivo.Text += "No es bmp";
+                return false;
+            }
+
+        }
+
+
+
+        private int tamaño(FileStream archivo)
+        {
+
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(2, SeekOrigin.Begin);//2 posición en un archivo bmp para el tamaño de bytes
+            bytes= br.ReadInt32();
+            Console.WriteLine("Tamaño real en bytes: " +  bytes);
+            txtArchivo.Text += "Tamaño real en bytes: " + bytes;
+            return bytes;
+        }
+        private int axaImagen(FileStream archivo)
+        {
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(18, SeekOrigin.Begin);//18 posición en un archivo bmp para el ancho de una imagen
+            ancho = br.ReadInt32();
+            Console.WriteLine("Ancho:{0} ", ancho);
+            txtArchivo.Text += Environment.NewLine + "Ancho de la imagen: " + ancho;
+            alto = br.ReadInt32();
+            Console.WriteLine("Alto:{0} ", alto);
+            txtArchivo.Text += Environment.NewLine + "Alto de la imagen: " + alto;
+            return ancho+alto;
+
+        }
+
+        private int bitsxPixel(FileStream archivo)
+        {
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(28, SeekOrigin.Begin);//28 posición en un archivo bmp para el bits por pixel
+            bxp = br.ReadInt16();
+            Console.WriteLine("Bits por pixel:{0} ", bxp);
+            txtArchivo.Text += Environment.NewLine + "Bits por pixel: " + bxp;
+            return bxp;
+        }
+
+
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {            
+            opnFileDialog.ShowDialog();
+            FileStream archivo = new FileStream(opnFileDialog.FileName, FileMode.Open);
+           
+
+            if (esBMP(archivo)==true)
+            {                
+                tamaño(archivo);
+                axaImagen(archivo);
+                bitsxPixel(archivo);
+            }
+            archivo.Close();
+        }
+
+        private void archivoXML( XmlDocument doc, string nombre, string apellido, string apellido2, string edad,string numeroCuenta)
+        {
+
+            
+            XmlDeclaration dcl= doc.CreateXmlDeclaration("1.0","utf-8","yes");
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore(dcl, root);
+
+            XmlNode node = doc.CreateElement("Alumnos");
+            doc.AppendChild(node);
+            XmlNode usNode = doc.CreateElement("usuario");
+            XmlAttribute age = doc.CreateAttribute("edad");
+            age.Value = edad;
+            usNode.Attributes.Append(age);
+            usNode.InnerText = nombre+" " +apellido + " " +apellido2 + " " +numeroCuenta ;
+            node.AppendChild(usNode);
+           
+
+        }
+
+        private void btnGuardarXML_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            sFileDialog.Filter = "XML files(.xml)|*.xml|all Files(*.*)|*.*";
+            sFileDialog.ShowDialog();
+
+            archivoXML( doc,txtNombre.Text, txtApellido1.Text, txtApellido2.Text, txtEdad.Text, txtNumCuenta.Text);
+
+            doc.Save(sFileDialog.FileName);
+
+            
+
+
+
+        }
+
+    }
+}
